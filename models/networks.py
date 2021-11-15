bca69aa1bd6329a0186188c53d501522d9f24fef
Kyuchul Lee <40943064+kyugorithm@users.noreply.github.com>
2021-11-15 23:59:10 +0900

    Update networks.py
    Fix : Unet discriminator structure

--- models/networks.py
+++ models/networks.py
@@ -709,13 +709,52 @@ class UpBlock(nn.Module):
         return x
 
 
+class DownBlock(nn.Module):
+    def __init__(self, input_channels, filters, downsample=True):
+        super().__init__()
+        self.conv_res = nn.Conv2d(input_channels, filters, 1, stride = (2 if downsample else 1))
+
+        self.net = double_conv(input_channels, filters)
+        self.down = nn.Conv2d(filters, filters, 3, padding = 1, stride = 2) if downsample else None
+
+    def forward(self, x):
+        # res = self.conv_res(x)
+        x = self.net(x)
+        unet_res = x
+
+        if self.down is not None:
+            x = self.down(x)
+
+        # x = x + res
+        return x, unet_res
+
+
+class UpBlock(nn.Module):
+    def __init__(self, input_channels, filters):
+        super().__init__()
+        # self.conv_res = nn.ConvTranspose2d(input_channels // 2, filters, 1, stride = 2)
+        self.net = double_conv(input_channels, filters)
+        self.up = nn.Upsample(scale_factor = 2, mode='bilinear', align_corners=False)
+        self.input_channels = input_channels
+        self.filters = filters
+
+    def forward(self, x, res):
+        *_, h, w = x.shape
+        # conv_res = self.conv_res(x, output_size = (h * 2, w * 2))
+        x = self.up(x)
+        x = torch.cat((x, res), dim=1)
+        x = self.net(x)
+        # x = x + conv_res
+        return x
+
+
 class UnetDiscriminator(nn.Module):
-    def __init__(self, image_size, network_capacity = 16, transparent = False, fmap_max = 512):
+    def __init__(self, image_size, network_capacity=64, transparent = False, fmap_max = 512):
         super().__init__()
         num_layers = int(log2(image_size) - 3)
         num_init_filters = 3 if not transparent else 4
 
-        blocks = []
+        # blocks = []
         filters = [num_init_filters] + [(network_capacity) * (2 ** i) for i in range(num_layers + 1)]
 
         set_fmap_max = partial(min, fmap_max)
@@ -726,28 +765,29 @@ class UnetDiscriminator(nn.Module):
         chan_in_out = list(map(list, chan_in_out))
 
         down_blocks = []
-        attn_blocks = []
+        # attn_blocks = []
 
         for ind, (in_chan, out_chan) in enumerate(chan_in_out):
-            num_layer = ind + 1
+            # num_layer = ind + 1
             is_not_last = ind != (len(chan_in_out) - 1)
 
             block = DownBlock(in_chan, out_chan, downsample = is_not_last)
             down_blocks.append(block)
 
-            attn_fn = attn_and_ff(out_chan)
-            attn_blocks.append(attn_fn)
+            # attn_fn = attn_and_ff(out_chan)
+            # attn_blocks.append(attn_fn)
 
         self.down_blocks = nn.ModuleList(down_blocks)
-        self.attn_blocks = nn.ModuleList(attn_blocks)
+        # self.attn_blocks = nn.ModuleList(attn_blocks)
 
         last_chan = filters[-1]
 
         self.to_logit = nn.Sequential(
             leaky_relu(),
-            nn.AvgPool2d(image_size // (2 ** num_layers)),
-            Flatten(1),
-            nn.Linear(last_chan, 1)
+            # nn.AvgPool2d(image_size // (2 ** num_layers)),
+            # Flatten(1),
+            # nn.Linear(last_chan, 1)
+            nn.Conv2d(last_chan,1,1)
         )
 
         self.conv = double_conv(last_chan, last_chan)
@@ -757,16 +797,17 @@ class UnetDiscriminator(nn.Module):
         self.conv_out = nn.Conv2d(3, 1, 1)
         
     def forward(self, x):
-        b, *_ = x.shape
+        # b, *_ = x.shape
 
         residuals = []
 
-        for (down_block, attn_block) in zip(self.down_blocks, self.attn_blocks):
+        # for (down_block, attn_block) in zip(self.down_blocks, self.attn_blocks):
+        for down_block in self.down_blocks:
             x, unet_res = down_block(x)
             residuals.append(unet_res)
 
-            if attn_block is not None:
-                x = attn_block(x)
+            # if attn_block is not None:
+            #     x = attn_block(x)
 
         x = self.conv(x) + x
         enc_out = self.to_logit(x)
@@ -776,6 +817,7 @@ class UnetDiscriminator(nn.Module):
 
         dec_out = self.conv_out(x)
         return enc_out.squeeze(), dec_out
+
 
 def random_hflip(tensor, prob):
     if prob > random():
